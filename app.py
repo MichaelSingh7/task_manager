63ff98c6b9e201a371b2704e43ccb54a42f8a6be
MichaelSingh7 <mikael.1@live.co.uk>
2019-11-12 09:50:53 +0000

    Created Template Files For My App

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
@@ -14,8 +15,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks(find())
-
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
