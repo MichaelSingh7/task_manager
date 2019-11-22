fc7b8561b2118c77c8d250d25958883bd98553a1
MichaelSingh7 <mikael.1@live.co.uk>
2019-11-22 14:22:22 +0000

    Added Submit Button And Add Task Functionality

--- app.py
+++ app.py
@@ -20,7 +20,15 @@ def get_tasks():
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+                           catagories=mongo.db.catagories.find())
+
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
